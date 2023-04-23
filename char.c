14b8b511e581b6b2bb4401a4f7a98fc623572ccf
yasoug <ougouisyassine@gmail.com>
2023-04-23 05:11:02 -0700

    task 0 : commit 3

--- char.c
+++ char.c
@@ -7,17 +7,10 @@
 char *print_c(va_list list)
 {
 	char *s;
-	char c;
-
-	c = va_arg(list, int);
-
-	if (c == 0)
-		c = '\0';
 
 	s = malloc(sizeof(char) * 2);
-	if (s == NULL)
-		return (NULL);
-	s[0] = c;
+
+	s[0] = va_arg(list, int);
 	s[1] = '\0';
 
 	return (s);
