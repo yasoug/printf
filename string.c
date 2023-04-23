eba5430ce8873be0b3b68cdf19331f93d1bfc395
yasoug <ougouisyassine@gmail.com>
2023-04-23 04:54:55 -0700

    task 0 : commit 1

--- string.c
+++ string.c
@@ -9,8 +9,18 @@
 char *print_s(va_list list)
 {
 	char *s;
+	char *p;
+	int len;
 
 	s = va_arg(list, char *);
+	if (s == NULL)
+		s = "(null)";
 
-	return (s);
+	len = _strlen(s);
+
+	p = malloc(sizeof(char) * len + 1);
+	if (p == NULL)
+		return (NULL);
+
+	return (_strcpy(p, s));
 }
